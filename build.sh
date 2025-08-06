d282b47e515f74f000e39c75f451b58fe4450fd6
Shivani Mishra <mishrashivani8780@gmail.com>
2025-08-06 10:14:21 +0530

    Implement static PDF serving for Render deployment

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py setup_static_pdfs
 
+
 # if [[ $CREATE_SUPERUSER ]]; then
 #     python manage.py createsuperuser --no-input
 # fi
